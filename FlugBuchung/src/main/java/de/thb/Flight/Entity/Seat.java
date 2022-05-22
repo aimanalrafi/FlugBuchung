d95f5ab75cd22242c09462f90576b2e2c9f9a3f3
DavidTotokra <102921842+DavidTotokra@users.noreply.github.com>
2022-05-22 13:55:07 +0200

    Add @

--- FlugBuchung/src/main/java/de/thb/Flight/Entity/Seat.java
+++ FlugBuchung/src/main/java/de/thb/Flight/Entity/Seat.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -20,6 +21,7 @@ public class Seat {
     int id;
     //Seat Nummer
     int nummer;
-    Flight flight;
 
+    @OneToOne
+    private Ticket ticket;
 }
