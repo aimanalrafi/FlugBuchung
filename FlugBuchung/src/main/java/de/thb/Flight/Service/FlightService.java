4e1f3bab17e510166db9ab404702b42e8e0ac9ec
aimanalrafi <aimansani55@gmail.com>
2022-05-29 12:02:19 +0200

    - Completed FlightRepository.java & FlightService.java - Added new attribute price to both Seat & Flight Entities

--- FlugBuchung/src/main/java/de/thb/Flight/Service/FlightService.java
+++ FlugBuchung/src/main/java/de/thb/Flight/Service/FlightService.java
@@ -1,5 +1,13 @@
 package de.thb.Flight.Service;
 
+import de.thb.Flight.Entity.Flight;
+import de.thb.Flight.Repository.FlightRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
 /**
  * Services should define all the CRUD functions of a specific class (objects)
  * - Create
@@ -23,6 +31,68 @@ package de.thb.Flight.Service;
  *
  */
 
+@Service
+@RequiredArgsConstructor
 public class FlightService {
+
+    public final FlightRepository flightRepository;
+
+    public List<Flight> getAllFlights(){
+        return (List<Flight>) flightRepository.findAll();
+    }
+
+    public Optional<Flight> getFlightById(long id){
+        return flightRepository.findById(id);
+    }
+
+    public List<Flight> getFlightByAbflugsort(String abflugsort){
+        if(abflugsort != null){
+            return flightRepository.searchFlightByAbflugsort(abflugsort);
+        } else {
+            throw new RuntimeException("Typo? Abflugsort doesn't exist");
+        }
+    }
+
+    public List<Flight> getFlightByZielort(String Zielort){
+        if(Zielort != null){
+            return flightRepository.searchFlightByZielort(Zielort);
+        } else {
+            throw new RuntimeException("Typo? Zielort doesn't exist");
+        }
+    }
+
+    public List<Flight> getFlightByDepartureDate(Date departureDate){
+        if(departureDate != null){
+            return flightRepository.searchFlightByDepartureDateAsc(departureDate);
+        }else {
+            throw new RuntimeException("Typo? Departure Date doesn't exist");
+        }
+    }
+
+    public List<Flight> getFlightByPriceAsc(){
+        return flightRepository.searchFlightByPriceAsc();
+    }
+
+    public List<Flight> getFlightByPriceDesc(){
+        return flightRepository.searchFlightByPriceDesc();
+    }
+
+    /**
+     * Admin functions
+     */
+    public void addFlight(Flight flight){
+        flightRepository.save(flight);
+    }
+
+    public void deleteFlightById(long id){
+        flightRepository.deleteById(id);
+    }
+
+    //returns number of flights in DB
+    public long CountFlights(){
+        return flightRepository.count();
+    }
+
+
 
 }
