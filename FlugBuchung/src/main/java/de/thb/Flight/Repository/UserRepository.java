9dac352967d32aed8089ffb729106d35231e3293
kheshan87 <38192837+kheshan87@users.noreply.github.com>
2022-05-30 18:22:00 +0200

    user service new

--- FlugBuchung/src/main/java/de/thb/Flight/Repository/UserRepository.java
+++ FlugBuchung/src/main/java/de/thb/Flight/Repository/UserRepository.java
@@ -7,17 +7,16 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.RepositoryDefinition;
 
-
-
+import java.util.List;
 
 
 public interface UserRepository extends JpaRepository<User, Long> {
     @Query("SELECT m from User m WHERE m.vorname=:vorname")
-    User getUserByVorname(String vorname);
+    List<User> searchUserByVorname(String vorname);
 
 
-    @Query("SELECT m from User m WHERE m.Nachname=:Nachname")
-    User getUserByNachname(String Nachname);
+    @Query("SELECT m from User m WHERE m.nachname=:nachname")
+    List<User> searchUserByNachname(String Nachname);
 
 
 
