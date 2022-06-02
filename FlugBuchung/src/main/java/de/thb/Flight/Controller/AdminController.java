6c1da98ce174d04034fbe922d987a101e0a966cd
DavidTotokra <102921842+DavidTotokra@users.noreply.github.com>
2022-06-02 16:24:07 +0200

    update

--- FlugBuchung/src/main/java/de/thb/Flight/Controller/AdminController.java
+++ FlugBuchung/src/main/java/de/thb/Flight/Controller/AdminController.java
@@ -1,4 +1,68 @@
+
 package de.thb.Flight.Controller;
 
+import de.thb.Flight.Entity.User;
+import de.thb.Flight.Service.FlightService;
+import de.thb.Flight.Service.SeatService;
+import de.thb.Flight.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.*;
+
+
+@Controller
+@RequestMapping("Admin")
+
 public class AdminController {
+
+
+
+
+
+
+    @Autowired
+    FlightService flightService;
+
+    @Autowired
+    SeatService seatService ;
+
+    @Autowired
+    UserService userService;
+
+
+    @GetMapping("/addUser")
+    public String viewAddUSerForm(Model model) {
+        model.addAttribute("user", new User());
+        return "AdminAddUser";
+    }
+
+    @PostMapping("/addUser")
+    @Validated
+    public String addUserByAdmin(@Valid User user, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "AdminAddUser";
+        }
+
+        if (userService.isUserPresent(user.getEmail())) {
+            model.addAttribute("exist", true);
+            return "AdminAddUser";
+        }
+        userService.creatUser(user);
+        model.addAttribute("success", true);
+        try {
+
+            //        emailService.registrationConfirmationEmail(user);
+        } catch (MailException ignored) {
+
+        }
+        return "redirect:/Admin/viewAdminPage";
+    }
+
 }
